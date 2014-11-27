81af235a1d5ce24e4ae6a19f68a7307fd3fea535
olibenni <olibenni@gmail.com>
2014-11-27 13:20:26 +0000

    Reglur, eyddi img.rar
    Bætti inn beinagrind að reglum

--- views/rules.php
+++ views/rules.php
@@ -1,5 +1,60 @@
 <main>
 	<div class="content">
 		<h2>REGLUR</h2>
+		<p>
+			Leikmaðurinn byrjar á að draga 4 spil og geyma á hendi sér.
+
+			Markmið leiksins er að losa sig við spil og sigur næst með því að vera með 2 spil eða færri á hendi.
+			Hægt er að losa sig við spil á tvennan hátt:<br>
+				ef spil með 2 spilum á milli sín hafa sömu sort er hægt að henda spilunum á milli<br>
+				ef spil með 2 spilum á milli sín hafa sama gildið er hægt að henda öllum 4 spilunum.<br><br>
+			<br>
+			
+
+			Dæmi til að losa sig við 2 spil:<br>
+			<img class='img' id='examplecard' src='/pandakapall/pandakapall/img/H3.png' height='100px' width='80px'>
+			<img class='img' id='examplecard' src='/pandakapall/pandakapall/img/S7.png' height='100px' width='80px'>
+			<img class='img' id='examplecard' src='/pandakapall/pandakapall/img/C5.png' height='100px' width='80px'>
+			<img class='img' id='examplecard' src='/pandakapall/pandakapall/img/H10.png' height='100px' width='80px'>
+			<br>
+			<br>
+			Hér eru spilin sem hægt er að losa sig við:<br>
+			<img class='img' id='examplecard' src='/pandakapall/pandakapall/img/S7.png' height='100px' width='80px'>
+			<img class='img' id='examplecard' src='/pandakapall/pandakapall/img/C5.png' height='100px' width='80px'>
+			<br>
+			<br>
+			Þar sem <br>
+			<img class='img' id='examplecard' src='/pandakapall/pandakapall/img/H3.png' height='100px' width='80px'>
+			<img class='img' id='examplecard' src='/pandakapall/pandakapall/img/H10.png' height='100px' width='80px'>
+			<bR>
+			eru af sömu sort.
+			<br>
+			Alltaf skal velja fyrsta spil sem á að fjarlægja. Semsagt:
+			<br>
+			<img class='img' id='examplecard' src='/pandakapall/pandakapall/img/S7.png' height='100px' width='80px'>
+
+			<br>
+			<br>
+			Dæmi til að losa sig við 4 spil:<br>
+			<img class='img' id='examplecard' src='/pandakapall/pandakapall/img/H3.png' height='100px' width='80px'>
+			<img class='img' id='examplecard' src='/pandakapall/pandakapall/img/S7.png' height='100px' width='80px'>
+			<img class='img' id='examplecard' src='/pandakapall/pandakapall/img/C5.png' height='100px' width='80px'>
+			<img class='img' id='examplecard' src='/pandakapall/pandakapall/img/D3.png' height='100px' width='80px'>
+			<br>
+			<br>
+			Hér er hægt að losa sig við öll 4 spilin.
+			<br>
+			<br>
+			Þar sem <br>
+			<img class='img' id='examplecard' src='/pandakapall/pandakapall/img/H3.png' height='100px' width='80px'>
+			<img class='img' id='examplecard' src='/pandakapall/pandakapall/img/D3.png' height='100px' width='80px'>
+			<bR>
+			eru með sama gildið.
+			<br>
+			Alltaf skal velja fyrsta spil sem á að fjarlægja. Semsagt:
+			<br>
+			<img class='img' id='examplecard' src='/pandakapall/pandakapall/img/H3.png' height='100px' width='80px'>
+
+		</p>
 	</div>
 </main>
